77a7dca9692b186cb499ca63177b54c0101c1c85
Andres <andresjml@gmail.com>
2021-07-07 11:54:46 -0400

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,68 @@
 # your code goes here
+class Person
+    def initialize(name)
+        @name=name
+        @bank_account=25
+        @happiness=8
+        @hygiene=8
+    end
+
+    attr_reader :name , :happiness, :hygiene    
+    attr_accessor :bank_account
+    
+    def happiness=(value)
+        @happiness = value
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 0
+    end
+        
+    def hygiene=(value)
+        @hygiene = value
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene < 0 
+    end
+    
+    def happy?
+        @happiness>7? true : false
+    end
+    
+    def clean?
+        @hygiene>7? true : false
+    end
+    
+    def get_paid(salary)
+        @bank_account=@bank_account+salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene+=4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene-=3
+        self.happiness+=2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(person)
+        person.happiness+=3
+        self.happiness+=3
+        "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        objects = [self, person]
+        if topic == "politics"
+          objects.each { |o| o.happiness -= 2}
+          first, second = ["partisan", "lobbyist"]
+        elsif topic == "weather"
+          objects.each { |o| o.happiness += 1}
+          first, second = ["sun", "rain"]
+        end
+        first ||= "blah"
+        second ||= "blah"
+        base_string = "blah blah #{first} blah #{second}"
+      end
+end
